4347857b19acb5835b65d1209fb77e7ae13eafa4
Arvinder <arvinder_kamboo@hotmail.com>
2020-09-14 21:22:28 +0100

    Section 3

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
